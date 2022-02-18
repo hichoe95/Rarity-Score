299d7f59ace0a7316357fe48a17c690a8629830e
hwanilchoi <hichoe95@naver.com>
2022-02-19 00:15:20 +0900

    2022/02/19

--- creativity_metric.py
+++ creativity_metric.py
@@ -16,7 +16,6 @@ class CREATIVITY(object):
 		self.num_fakes = fake_features.shape[0]
 
 		print('Preprocessing pairwise diatances ...')
-		self.real2fake_distances = compute_pairwise_distances(real_features, fake_features)
 		self.real2real_distances = compute_pairwise_distances(real_features)
 
 		self.real2real_sorted = np.sort(self.real2real_distances, axis = 1)
@@ -28,10 +27,8 @@ class CREATIVITY(object):
 		self.sample_mode_ids = None
 
 		self.mode2mode_distances = None
-		self.mode2fake_distances = None
 		self.mode2mode_sorted = None
 		self.mode2mode_sorted_ids = None
-
 
 	def is_in_ball(self, k = 3, samples = None, cluster = False):
 		""" Compute the differences between radii of kNN balls and distances
@@ -48,17 +45,14 @@ class CREATIVITY(object):
 				r (np.array, num_reals * 1): radii of each kNN real balls
 				out_ball_ids (np.array, num_out_ball_samples): indices of samples outside of balls.
 		"""
-
-		if samples is None:
-			samples = self.fake_features
-			real2samples_distances = self.real2fake_distances if not cluster else self.mode2fake_distances
+		if not cluster:
+			real2samples_distances = compute_pairwise_distances(self.real_features, samples)
 		else:
-			real2samples_distances = compute_pairwise_distances(self.real_features, samples) if not cluster else \
-			compute_pairwise_distances(self.modes, samples)
+			real2samples_distances = compute_pairwise_distances(self.modes, samples)
 
-		r = self.real2real_sorted[:,k:k+1] if not cluster else self.mode2mode_sorted[:, k:k+1]
+		r = self.real2real_sorted[:,k+1] if not cluster else self.mode2mode_sorted[:, k+1]
 
-		dist_radi = (r.repeat(samples.shape[0], axis = 1) - real2samples_distances)
+		dist_radi = (r[:,None].repeat(samples.shape[0], axis = 1) - real2samples_distances)
 		out_ball_ids = np.where((dist_radi > 0).any(axis = 0) == False)[0]
 
 		return dist_radi, r, out_ball_ids
@@ -126,30 +120,6 @@ class CREATIVITY(object):
 		return scores, scores_ids
 
 
-	def clustering_kmeans(self, k = 3, num_cluster = 300, samples = None, verbose = 1):
-		""" Implementing kmeans clustering with real features and 
-			preprocessing some varialbes.
-			
-			args:
-				k (int): a mode ball's size is distance between the mode and the k th nearest mode.
-				num_cluster (int): the number of classes how many you want to divide.
-				samples (np.array, N * embed_dim): embedded generation samples
-		"""
-		samples = self.fake_features if samples is None else samples
-
-		if self.num_cluster != num_cluster:
-			print("Now, clustering is in progress...")
-			cluster = kmeans(n_clusters = num_cluster, verbose = verbose).fit(self.real_features)
-			self.modes = cluster.cluster_centers_
-			self.sample_mode_ids = cluster.labels_
-
-			self.mode2mode_distances = compute_pairwise_distances(self.modes)
-			self.mode2fake_distances = compute_pairwise_distances(self.modes, samples)
-			self.mode2mode_sorted = np.sort(self.mode2mode_distances, axis = 1)
-			self.mode2mode_sorted_ids = self.mode2mode_distances.argsort(axis = 1)
-			print("Clustering is done!")
-		else:
-			print("Clustering is already processed")
 
 	def metric3(self, k = 15, num_cluster = 300, samples = None):
 		""" When there are multiple mode balls that contain a fake sample,
@@ -204,24 +174,47 @@ class CREATIVITY(object):
 
 		self.clustering_kmeans(k = k, num_cluster = num_cluster, samples = samples)
 
-		in_ball_dist, _, __ = self.is_in_ball(k = k, samples = self.real_features, cluster = True)
-		num_reals_in_mode_ball = (in_ball_dist > 0).sum(axis = 1)
-
+		_, num_reals_in_mode = np.unique(self.sample_mode_ids, return_counts = True)
 		in_ball_dist_sample, _, out_ball_ids = self.is_in_ball(samples = samples, k = k, cluster = True)
 		num_out_ball = len(out_ball_ids)
 
-		modes_ids, samples_ids = np.where(in_ball_dist_sample > 0)
-		_, cnt = np.unique(samples_ids, return_counts = True)
+		knn_mode_ids = self.mode2mode_sorted_ids[:,:k+1]
+		mode_weight = np.array([1./num_reals_in_mode[knn_mode_ids[i]].sum() for i in range(self.modes.shape[0])])
 
 		scores = np.zeros(samples.shape[0])
 		for i in range(samples.shape[0]):
-			mode_ids = modes_ids[:cnt[i]] if i == 0 else modes_ids[cnt[i-1]: cnt[i]]
-			scores[i] = (1. / num_reals_in_mode_ball[mode_ids]).sum()
+			mode_ids = np.where(in_ball_dist_sample[:,i] > 0)[0]
+			if len(mode_ids) is not 0:
+				scores[i] = ((mode_weight[mode_ids]).sum())
+
 
 		scores_ids = (-scores).argsort()[:samples.shape[0] - num_out_ball]
 
 		return scores, scores_ids
 
+	def clustering_kmeans(self, samples, k = 3, num_cluster = 300, verbose = 1):
+		""" Implementing kmeans clustering with real features and 
+			preprocessing some varialbes.
+			
+			args:
+				k (int): a mode ball's size is distance between the mode and the k th nearest mode.
+				num_cluster (int): the number of classes how many you want to divide.
+				samples (np.array, N * embed_dim): embedded generation samples
+		"""
+
+		if self.num_cluster != num_cluster:
+			print("Now, clustering is in progress...")
+			cluster = kmeans(n_clusters = num_cluster, verbose = verbose, n_init = 10).fit(self.real_features)
+			self.num_cluster = num_cluster
+			self.modes = cluster.cluster_centers_
+			self.sample_mode_ids = cluster.labels_
+
+			self.mode2mode_distances = compute_pairwise_distances(self.modes)
+			self.mode2mode_sorted = np.sort(self.mode2mode_distances, axis = 1)
+			self.mode2mode_sorted_ids = self.mode2mode_distances.argsort(axis = 1)
+			print("Clustering is done!")
+		else:
+			print("Clustering is already processed")
 
 
 
